d7cfe45201efed65c0cbc930388ee18f3f5c4acd
Eduards Sizovs <eduards@sizovs.net>
2024-11-18 15:48:42 +0800

    auto scan @ConfigurationProperties

--- src/main/java/cleanbank/Application.java
+++ src/main/java/cleanbank/Application.java
@@ -1,16 +1,15 @@
 package cleanbank;
 
-import cleanbank.domains.accounts.WithdrawalLimits;
 import cleanbank.infra.jpa.BaseJpaRepository;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties(WithdrawalLimits.class)
+@ConfigurationPropertiesScan
 @EnableScheduling
 @EnableJpaRepositories(repositoryBaseClass = BaseJpaRepository.class)
 public class Application {
