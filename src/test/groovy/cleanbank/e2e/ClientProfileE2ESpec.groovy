bf6a6068b0ed3a10d3cbedffb06960359c8c08e6
Eduards Sizovs <eduards@sizovs.net>
2024-11-19 13:49:11 +0800

    add user profile retrival e2e test

--- src/test/groovy/cleanbank/e2e/ClientProfileE2ESpec.groovy
+++ src/test/groovy/cleanbank/e2e/ClientProfileE2ESpec.groovy
@@ -4,7 +4,7 @@ import cleanbank.e2e.commons.E2ESpec
 
 class ClientProfileE2ESpec extends E2ESpec {
 
-  def "populated user profile"() {
+  def "get user profile"() {
     given:
     def someone = person()
 
