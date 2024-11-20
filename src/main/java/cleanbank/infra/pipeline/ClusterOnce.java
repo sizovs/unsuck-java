1d96d04e60ea6480891a31bc9af6252a4e51b526
Eduards Sizovs <eduards@sizovs.net>
2024-11-20 13:58:15 +0800

    ClusterOnce: bye-bye tx template; reactions run in tx anyway

--- src/main/java/cleanbank/infra/pipeline/ClusterOnce.java
+++ src/main/java/cleanbank/infra/pipeline/ClusterOnce.java
@@ -2,8 +2,6 @@ package cleanbank.infra.pipeline;
 
 import cleanbank.infra.locks.AdvisoryLock;
 import cleanbank.infra.spring.annotations.PrototypeScoped;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
 
 public class ClusterOnce<C extends Command<R>, R> implements Command<Command.Void> {
 
@@ -18,23 +16,17 @@ public class ClusterOnce<C extends Command<R>, R> implements Command<Command.Voi
   @PrototypeScoped
   static class Reaction<C extends Command<R>, R> implements Command.Reaction<ClusterOnce<C, R>, Void> {
 
-    private final PlatformTransactionManager transactionManager;
     private final AdvisoryLock lock;
 
-    Reaction(PlatformTransactionManager transactionManager, AdvisoryLock lock) {
-      this.transactionManager = transactionManager;
+    Reaction(AdvisoryLock lock) {
       this.lock = lock;
     }
 
     @Override
     public Void react(ClusterOnce<C, R> cmd) {
-      var tx = new TransactionTemplate(transactionManager);
-      tx.setName(cmd.taskId);
-      tx.executeWithoutResult(status -> {
-        if (lock.acquire(cmd.taskId)) {
-          cmd.origin.now();
-        }
-      });
+      if (lock.acquire(cmd.taskId)) {
+        cmd.origin.now();
+      }
 
       return new Void();
     }
