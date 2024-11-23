a748c349247df230c5c58c4c3b0aee186fecdac0
Eduards Sizovs <eduards@sizovs.net>
2024-11-23 20:51:23 +0800

    bumb up spring boot (3.3.5 -> 3.4.0)

--- src/main/java/cleanbank/commands/SendPromoOfferings.java
+++ src/main/java/cleanbank/commands/SendPromoOfferings.java
@@ -1,6 +1,6 @@
 package cleanbank.commands;
 
-import cleanbank.infra.pipeline.ClusterOnce;
+import cleanbank.infra.pipeline.Lock;
 import cleanbank.infra.pipeline.Command;
 import cleanbank.infra.spring.annotations.PrototypeScoped;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -23,7 +23,7 @@ public class SendPromoOfferings implements Command<Command.Void> {
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void run() {
-      new ClusterOnce<>(
+      new Lock<>(
         new SendPromoOfferings()
       ).now();
     }
--- src/main/java/cleanbank/infra/pipeline/Lock.java
+++ src/main/java/cleanbank/infra/pipeline/Lock.java
@@ -3,18 +3,18 @@ package cleanbank.infra.pipeline;
 import cleanbank.infra.locks.AdvisoryLock;
 import cleanbank.infra.spring.annotations.PrototypeScoped;
 
-public class ClusterOnce<C extends Command<R>, R> implements Command<Command.Void> {
+public class Lock<C extends Command<R>, R> implements Command<Command.Void> {
 
   private final int lockId;
   private final C origin;
 
-  public ClusterOnce(C origin) {
+  public Lock(C origin) {
     this.lockId = origin.getClass().getSimpleName().hashCode();
     this.origin = origin;
   }
 
   @PrototypeScoped
-  static class Reaction<C extends Command<R>, R> implements Command.Reaction<ClusterOnce<C, R>, Void> {
+  static class Reaction<C extends Command<R>, R> implements Command.Reaction<Lock<C, R>, Void> {
 
     private final AdvisoryLock lock;
 
@@ -23,7 +23,7 @@ public class ClusterOnce<C extends Command<R>, R> implements Command<Command.Voi
     }
 
     @Override
-    public Void react(ClusterOnce<C, R> cmd) {
+    public Void react(Lock<C, R> cmd) {
       if (lock.acquire(cmd.lockId)) {
         cmd.origin.now();
       }
