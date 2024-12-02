3a2723b0a58c9ca7aa138462a11fb26c2ffdf89d
Eduards Sizovs <eduards@sizovs.net>
2024-12-02 11:25:18 +0800

    ditch unnecessary @Component annotation for repos

--- src/main/java/cleanbank/domains/crm/Clients.java
+++ src/main/java/cleanbank/domains/crm/Clients.java
@@ -1,12 +1,10 @@
 package cleanbank.domains.crm;
 
 import org.springframework.data.repository.Repository;
-import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Component
 public interface Clients extends Repository<Client, UUID> {
 
   Client findById(UUID clientId);
