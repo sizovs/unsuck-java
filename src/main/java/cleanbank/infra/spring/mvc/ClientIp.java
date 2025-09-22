010e607f91b1db8e371f9b97486123a0462f8a59
Eduards Sizovs <eduards@sizovs.net>
2025-09-22 15:12:11 +0800

    use ScopedValue for ClientIp

--- src/main/java/cleanbank/infra/spring/mvc/ClientIp.java
+++ src/main/java/cleanbank/infra/spring/mvc/ClientIp.java
@@ -9,34 +9,32 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @Component
 public class ClientIp {
 
-  private static final ThreadLocal<Optional<String>> CLIENT_IP = InheritableThreadLocal.withInitial(Optional::empty);
+  private static final ScopedValue<String> CLIENT_IP = ScopedValue.newInstance();
 
   public boolean isAvailable() {
-    return CLIENT_IP.get().isPresent();
+    return CLIENT_IP.isBound();
   }
 
   @Override
   public String toString() {
-    return CLIENT_IP.get().orElseThrow(() -> new RuntimeException("Client ip is not available"));
+    return CLIENT_IP.orElseThrow(() -> new RuntimeException("Client ip is not available"));
   }
 
   @Component
   static class RequestFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain)
-      throws ServletException, IOException {
-      try {
-        var ip = Optional.of(request.getRemoteAddr());
-        CLIENT_IP.set(ip);
-        chain.doFilter(request, response);
-      } finally {
-        CLIENT_IP.remove();
-      }
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) {
+      ScopedValue.where(CLIENT_IP, request.getRemoteAddr()).run(() -> {
+        try {
+          chain.doFilter(request, response);
+        } catch (IOException | ServletException e) {
+          throw new RuntimeException(e);
+        }
+      });
     }
   }
 
